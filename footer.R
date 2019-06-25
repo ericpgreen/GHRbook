2f6847fed4102f2c2ec6e5827b5ba617f0dc0b86
Eric Green <epgreen@gmail.com>
2019-06-25 23:00:50 +0300

    update footer text size

--- footer.R
+++ footer.R
@@ -9,7 +9,7 @@ write_custom_footer <- function() {
     c("<div class=\"row\" style=\"padding-top: 2em;\">",
       "<p style=\"text-align: center\">",
       "<img src=\"images/logo_dark.png\" style=\"width: 100px; padding: 0; display: inline; vertical-align: top\">",
-      "<span style=\"display: inline-block; margin-left: 2em; margin-top: 10px;\">",
+      "<span style=\"display: inline-block; margin-left: 2em; margin-top: 16px; font-size: small\">",
       "<span style=\"font-weight: bold;\">Eric P. Green</span><br/>",
       "<a href=\"https://themethodsection.com\">themethodsection.com</a><br/>",
       paste("page built ", as.character(Sys.Date())),
